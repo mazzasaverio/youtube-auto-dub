f47775cd4245643cbadf4bcd867d4b7d1857a4e9
mazzasaverio <saverio3107@gmail.com>
2024-01-21 16:08:43 +0100

    UPDATE

--- src/main.py
+++ src/main.py
@@ -3,8 +3,9 @@ from sqlalchemy.orm import Session
 from loguru import logger
 from .database.connection import SessionLocal, engine
 from .database.models import Base, Item
-from .translation.text_translator import load_model, translate_text
-import os
+
+# from .translation.text_translator import load_model, translate_text
+# import os
 
 
 Base.metadata.create_all(bind=engine)
@@ -28,25 +29,25 @@ async def pubsub_handler():
         for item in items:
             logger.info(f"Retrieved item from database: {item.name}")
 
-    # Translation Operation
-    text = """Wie oben bereits erwähnt, ist die Transparenz ein entscheidender Treiber für diese Entwicklungen. Im Bereich
-              Emissionen wird laut House Gas unterschieden in Emissionen, welche dem Unternehmen
-              direkt zuzuordnen sind, z. B. die eingekaufte Energie und
-              die Prozessemissionen. Diese zusam"""
-    source_language = "de"
-    target_language = "en"
+    # # Translation Operation
+    # text = """Wie oben bereits erwähnt, ist die Transparenz ein entscheidender Treiber für diese Entwicklungen. Im Bereich
+    #           Emissionen wird laut House Gas unterschieden in Emissionen, welche dem Unternehmen
+    #           direkt zuzuordnen sind, z. B. die eingekaufte Energie und
+    #           die Prozessemissionen. Diese zusam"""
+    # source_language = "de"
+    # target_language = "en"
 
-    # Load model
-    model_name = "facebook/m2m100_418M"
-    storage_option = os.getenv("STORAGE_OPTION")
-    tokenizer, model = load_model(model_name, storage_option)
+    # # Load model
+    # model_name = "facebook/m2m100_418M"
+    # storage_option = os.getenv("STORAGE_OPTION")
+    # tokenizer, model = load_model(model_name, storage_option)
 
-    # Perform translation
-    translated_text = translate_text(
-        text, source_language, target_language, tokenizer, model
-    )
-    logger.info(f"Original text: {text}")
-    logger.info(f"Translated text: {translated_text}")
+    # # Perform translation
+    # translated_text = translate_text(
+    #     text, source_language, target_language, tokenizer, model
+    # )
+    # logger.info(f"Original text: {text}")
+    # logger.info(f"Translated text: {translated_text}")
 
     return {
         "message": "Pub/Sub handler executed successfully, database and translation tasks completed."
